5db0a78643fa4779c0490692feff06b52ba8d875
unknown <wangnan3.1415926@163.com>
2019-07-19 13:59:53 +0800

    use RC for 32768hz

--- examples/ble_peripheral/ble_app_bma250E/main.c
+++ examples/ble_peripheral/ble_app_bma250E/main.c
@@ -585,51 +585,51 @@ void bsp_event_handler(bsp_event_t event)
 /**@snippet [Handling the data received over UART] */
 void uart_event_handle(app_uart_evt_t * p_event)
 {
-    static uint8_t data_array[BLE_NUS_MAX_DATA_LEN];
-    static uint8_t index = 0;
-    uint32_t       err_code;
+//    static uint8_t data_array[BLE_NUS_MAX_DATA_LEN];
+//    static uint8_t index = 0;
+//    uint32_t       err_code;
 
-    switch (p_event->evt_type)
-    {
-        case APP_UART_DATA_READY:
-            UNUSED_VARIABLE(app_uart_get(&data_array[index]));
-            index++;
+//    switch (p_event->evt_type)
+//    {
+//        case APP_UART_DATA_READY:
+//            UNUSED_VARIABLE(app_uart_get(&data_array[index]));
+//            index++;
 
-            if (index >=7)//((data_array[index - 1] == '\n') || (data_array[index - 1] == '\r') || (index >= m_ble_nus_max_data_len))
-            {
-                if (index > 1)
-                {
-                    NRF_LOG_DEBUG("Ready to send data over BLE NUS");
-                    NRF_LOG_HEXDUMP_DEBUG(data_array, index);
+//            if (index >=7)//((data_array[index - 1] == '\n') || (data_array[index - 1] == '\r') || (index >= m_ble_nus_max_data_len))
+//            {
+//                if (index > 1)
+//                {
+//                    NRF_LOG_DEBUG("Ready to send data over BLE NUS");
+//                    NRF_LOG_HEXDUMP_DEBUG(data_array, index);
 
-                    do
-                    {
-                        uint16_t length = (uint16_t)index;
-                        err_code = ble_nus_data_send(&m_nus, data_array, &length, m_conn_handle);
-                        if ((err_code != NRF_ERROR_INVALID_STATE) &&
-                            (err_code != NRF_ERROR_RESOURCES) &&
-                            (err_code != NRF_ERROR_NOT_FOUND))
-                        {
-                            APP_ERROR_CHECK(err_code);
-                        }
-                    } while (err_code == NRF_ERROR_RESOURCES);
-                }
+//                    do
+//                    {
+//                        uint16_t length = (uint16_t)index;
+//                        err_code = ble_nus_data_send(&m_nus, data_array, &length, m_conn_handle);
+//                        if ((err_code != NRF_ERROR_INVALID_STATE) &&
+//                            (err_code != NRF_ERROR_RESOURCES) &&
+//                            (err_code != NRF_ERROR_NOT_FOUND))
+//                        {
+//                            APP_ERROR_CHECK(err_code);
+//                        }
+//                    } while (err_code == NRF_ERROR_RESOURCES);
+//                }
 
-                index = 0;
-            }
-            break;
+//                index = 0;
+//            }
+//            break;
 
-        case APP_UART_COMMUNICATION_ERROR:
-            APP_ERROR_HANDLER(p_event->data.error_communication);
-            break;
+//        case APP_UART_COMMUNICATION_ERROR:
+//            APP_ERROR_HANDLER(p_event->data.error_communication);
+//            break;
 
-        case APP_UART_FIFO_ERROR:
-            APP_ERROR_HANDLER(p_event->data.error_code);
-            break;
+//        case APP_UART_FIFO_ERROR:
+//            APP_ERROR_HANDLER(p_event->data.error_code);
+//            break;
 
-        default:
-            break;
-    }
+//        default:
+//            break;
+//    }
 }
 /**@snippet [Handling the data received over UART] */
 
